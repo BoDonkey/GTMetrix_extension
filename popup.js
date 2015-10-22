58b7739878f7d73da534d8cba3af15eac488ff21
BoDonkey <robert.means1969@gmail.com>
2015-10-22 11:13:27 -0400

    Create first chrome extension

---
+++ popup.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var checkPageButton = document.getElementById('checkPage');
+  checkPageButton.addEventListener('click', function() {
+	chrome.tabs.query({currentWindow: true, active: true}, function(tab) {
+		d = document;
+
+      var f = d.createElement('form');
+      f.action = 'http://gtmetrix.com/analyze.html?bm';
+      f.method = 'post';
+      var i = d.createElement('input');
+      i.type = 'hidden';
+      i.name = 'url';
+      i.value = tab[0].url;
+      f.appendChild(i);
+      d.body.appendChild(f);
+      f.submit();
+
+	});
+  }, false);
+}, false);
